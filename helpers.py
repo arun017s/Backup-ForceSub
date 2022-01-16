6e63d60c40f7d26a0a020e3485209190dc285c48
Arunkumar Shibu <86429222+Arun-TG@users.noreply.github.com>
2022-01-16 17:51:15 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -4,9 +4,12 @@
 
 import os
 import asyncio
-from pyrogram.errors import FloodWait
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.types import ChatPermissions
 
-CHANNEL = int(os.environ.get("CHANNEL"))
+DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
+FSUB_CHANNEL = int(os.environ.get("FSUB_CHANNEL"))
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK")
 
 LIST = ["srt", "txt", "jpg", "jpeg", "png", "torrent", "html", "aio", "pdf", "zip"]
 
@@ -22,4 +25,26 @@ async def copy_msg(msg):
            await asyncio.sleep(e.x)
            await msg.copy(CHANNEL)
            
+
+async def force_sub(bot, msg):
+    if msg.from_user.is_bot:
+       return
+    else:
+       try:
+          member = await bot.get_chat_member(FSUB_CHANNEL, msg.from_user.id)
+          if member.status == "banned":
+            await msg.reply(f"Sorry {msg.from_user.mention}!\n You are banned in our channel, you will be banned from here within 10 seconds")
+            await asyncio.sleep(10)
+            await bot.ban_chat_member(msg.chat.id, msg.from_user.id)
+       except UserNotParticipant:
+            await bot.restrict_chat_member(chat_id=msg.chat.id, 
+                                           user_id=msg.from_user.id,
+                                           permissions=ChatPermissions(can_send_messages=False)
+                                           )
+            await msg.reply(f"Hello {msg.from_user.mention}!\n\nYou have to join in our channel to message here", 
+                            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url=CHANNEL_LINK)]])
+                            )
+       except Exception as e:
+            print(e)
+
     
