aac1de407f957edd5cb1b43021bf408a69a29d4f
Arunkumar Shibu <86429222+Arun-TG@users.noreply.github.com>
2022-01-18 15:05:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID=int(os.environ.get("API_ID")),
+API_ID=os.environ.get("API_ID"),
 API_HASH=os.environ.get("API_HASH"),
 BOT_TOKEN=os.environ.get("BOT_TOKEN")
 SESSION=os.environ.get("SESSION")
