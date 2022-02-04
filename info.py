390274fd120b4447bbd0cdab5b41494b3457b871
Arunkumar Shibu <86429222+Arun-TG@users.noreply.github.com>
2022-02-04 23:34:39 +0530

    hm..?

--- info.py
+++ info.py
@@ -5,8 +5,11 @@ API_HASH=os.environ.get("API_HASH")
 BOT_TOKEN=os.environ.get("BOT_TOKEN")
 SESSION=os.environ.get("SESSION")
 
+GROUPS = []
+for grp in (os.environ.get("GROUPS").split()):
+    GROUPS.append(int(grp))
+
 AUTO_DELETE = os.environ.get("AUTO_DELETE")
-GROUPS = os.environ.get("GROUPS").split()
 DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
 FSUB_CHANNEL = int(os.environ.get("FSUB_CHANNEL"))
 CHANNEL_LINK = os.environ.get("CHANNEL_LINK")
